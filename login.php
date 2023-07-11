4d9dd7a4aa29ac91598f2197aad1ddeb84f005c5
WillTDP <77252197+WillTDP@users.noreply.github.com>
2023-07-11 17:37:09 +0200

    Feat: basic login works

--- login.php
+++ login.php
@@ -1,11 +1,29 @@
 <?php
- if (!empty($_POST)) {
-    $username = $_POST['username'];
+session_start();
 
+function canLogin($username, $password) {
+    if ($username == 'admin' && $password == 'admin') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+if (!empty($_POST)) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-?><!DOCTYPE html>
+    if (canLogin($username, $password)) {
+        $_SESSION['username'] = $username;
+        header('Location: index.php');
+        exit(); // Terminate the script after redirection
+    } else {
+        $error = 'Wrong username or password';
+    }
+}
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,20 +31,22 @@
     <title>Document</title>
 </head>
 <body>
-
-<div>
-    <form action="login.php" method="post">
-    <h2>Login</h2>
-    <div class="form form--login">
-        <label for="uid">Username</label>
-        <input type="text" id="username" name="username">
+    <div>
+        <form action="login.php" method="post">
+            <h2>Login</h2>
+            <?php if (isset($error)): ?>
+                <div class="alert"><?php echo $error; ?></div>
+            <?php endif; ?>
+            <div class="form form--login">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
+            </div>
+            <div class="form form--login">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <input type="submit" value="Login" id="login">
+        </form>
     </div>
-    <div class="form form--login">
-        <label for="pwd">Password</label>
-        <input type="password" id="password" name="password">
-    </div>
-
-    <input type="submit" value="Login" id="login">
-</div>
 </body>
 </html>
