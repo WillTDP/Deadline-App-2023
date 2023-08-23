36a1ce889167d97920080f6960bb6504a66a34f6
WillTDP <77252197+WillTDP@users.noreply.github.com>
2023-08-23 14:11:59 +0200

    Fix: fixed problem with db path

--- classes/Db.php
+++ classes/Db.php
@@ -4,7 +4,7 @@
     public static function connect() {
         include_once(__DIR__ . "/../settings/settings.php");
         if (empty(self::$conn)) {
-            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['db'], SETTINGS['db']['user'], SETTINGS['db']['password']);
+            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['dbname'], SETTINGS['db']['user'], SETTINGS['db']['password']);
             return self::$conn;
         } else {
             return self::$conn;
